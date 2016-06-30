41edf7cbd97f0d381bdcee637cb337504a5e2063
SergeyLobanov <sinet_ua@bk.ru>
2016-06-30 11:54:20 +0300

    Text Processing 0.99 clear

--- src/epam/TextProcessingMain.java
+++ src/epam/TextProcessingMain.java
@@ -14,6 +14,6 @@ public class TextProcessingMain {
     public static void main(String[] args) throws IOException {
         TextProcessing text = new TextProcessing(Controller.BOOK1_DIRECTORY, Controller.RUS_BOOK);
         Controller controller = new Controller(text, new View());
-        controller.processText();
+        controller.processText(); 
     }
 }
